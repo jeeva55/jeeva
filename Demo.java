c40d53b2ee09e6b3e1a643508a96e89c7c6a75ae
jeeva55 <47380548+jeeva55@users.noreply.github.com>
2019-02-06 15:40:57 +0530

    Create Demo.java

---
+++ Demo.java
@@ -0,0 +1,36 @@
+public class Demo
+
+{
+    public static void main(String[] args)
+ 
+    {
+ 
+     javascript:void(0)  int number=3;
+
+        if(number > 0)
+       
+ {
+ 
+           System.out.println(number+" is a positive ");
+  
+      }
+  
+      else if(number < 0)
+
+        {
+ 
+           System.out.println(number+" is a negative number");
+
+        }
+
+        else
+ 
+       {
+    
+        System.out.println(number+" is neither positive nor negative");
+
+        }
+
+    }
+
+}
